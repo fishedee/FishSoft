0f1972122d670f67ee97168bd289642b20c5d36b
fishedee <306766045@qq.com>
2014-12-17 19:10:54 +0800

    加入ubuntu14.04

---
+++ ubuntu14.04/base.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#更新库
+sudo apt-get update
+
+#安装基础编译器
+sudo apt-get install g++ -y
+sudo apt-get install openjdk-6-jdk -y
+
+#安装nginx-php-mysql一条龙
+sudo apt-get install apache2 -y
+sudo apt-get install nginx -y
+sudo apt-get install php5 -y
+sudo apt-get install php5-fpm -y
+sudo apt-get install php5-gd -y
+sudo apt-get install php5-imageick -y
+sudo apt-get install php5-imap -y
+sudo apt-get install php5-mcrypt -y
+sudo apt-get install php5-mysql -y
+sudo apt-get install php5-mysqlnd -y
+sudo apt-get install mysql-server-5.6 -y
+
+
+
+

62d175fab57c66e3cf41ab029957579792e311f5
fishedee <306766045@qq.com>
2015-04-21 14:57:46 +0800

    加入hhvm的支持

--- ubuntu14.04/base.sh
+++ ubuntu14.04/base.sh
@@ -25,3 +25,11 @@ sudo ln -s /usr/bin/nodejs /usr/sbin/node
 sudo apt-get install npm -y
 node -v
 npm -v
+
+#安装hhvm
+sudo apt-get install software-properties-common
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0x5a16e7281be7a449
+sudo add-apt-repository 'deb http://mirrors.hypo.cn/hhvm/ubuntu trusty main'
+sudo apt-get update
+sudo apt-get install hhvm
+
